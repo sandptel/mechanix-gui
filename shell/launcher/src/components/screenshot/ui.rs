630f301047bc29bfe92ad233e253ac0de0b85b33
sandptel <sandeeppatel626735@gmail.com>
2025-08-17 21:09:05 +0530

    feat: added buttons and save functionality

--- shell/launcher/src/components/screenshot/ui.rs
+++ shell/launcher/src/components/screenshot/ui.rs
@@ -3,7 +3,7 @@ use bevy_smithay::{
     prelude::{ layer_shell::LayerShellSettings, subsurface::Anchor },
 };
 use crate::launcher::spawn_camera;
-use super::plugin::{CurrentScreenshotImage, save_image_as_png};
+use super::plugin::CurrentScreenshotImage;
 use super::plugin::ScreenshotPlugin;
 
 #[derive(Component)]
@@ -30,6 +30,12 @@ pub struct DialogOverlay;
 #[derive(Component)]
 pub struct DialogTimer(pub Timer);
 
+#[derive(Component)]
+pub struct YesButton;
+
+#[derive(Component)]
+pub struct NoButton;
+
 pub struct ScreenshotUiPlugin;
 
 impl Plugin for ScreenshotUiPlugin {
@@ -45,6 +51,8 @@ impl Plugin for ScreenshotUiPlugin {
                 delete_button_interaction,
                 copy_button_interaction,
                 auto_hide_dialogs,
+                yes_button_interaction,
+                no_button_interaction,
             ))
             .add_plugins(ScreenshotPlugin);
     }
@@ -259,7 +267,6 @@ fn spawn_screenshot_ui(
 fn save_button_interaction(
     mut interaction_query: Query<&Interaction, (Changed<Interaction>, With<SaveButton>)>,
     current_screenshot: Option<Res<CurrentScreenshotImage>>,
-    images: Res<Assets<Image>>,
     mut commands: Commands,
     screenshot_window: Option<Res<ScreenshotWindowSurface>>,
     existing_dialog: Query<Entity, With<SaveDialog>>,
@@ -270,38 +277,14 @@ fn save_button_interaction(
                 println!("Save button was pressed");
                 
                 if let Some(current_screenshot) = &current_screenshot {
-                    if let Some(image) = images.get(&current_screenshot.image) {
-                        // First try to save the image
-                        match save_image_as_png(image, &current_screenshot.output_path) {
-                            Ok(()) => {
-                                println!("Screenshot saved successfully");
-                                
-                                // Remove any existing dialog first
-                                for entity in existing_dialog.iter() {
-                                    commands.entity(entity).despawn();
-                                }
-                                
-                                // Spawn save success dialog
-                                if let Some(window) = &screenshot_window {
-                                    spawn_save_dialog(&mut commands, window.0);
-                                }
-                            }
-                            Err(e) => {
-                                eprintln!("Failed to save screenshot: {}", e);
-                                
-                                // Remove any existing dialog first
-                                for entity in existing_dialog.iter() {
-                                    commands.entity(entity).despawn();
-                                }
-                                
-                                // Spawn error dialog
-                                if let Some(window) = &screenshot_window {
-                                    spawn_error_dialog(&mut commands, window.0, &format!("Failed to save: {}", e));
-                                }
-                            }
-                        }
-                    } else {
-                        eprintln!("Screenshot image not found in assets");
+                    // Remove any existing dialog first
+                    for entity in existing_dialog.iter() {
+                        commands.entity(entity).despawn();
+                    }
+                    
+                    // Spawn save confirmation dialog
+                    if let Some(window) = &screenshot_window {
+                        spawn_save_confirmation_dialog(&mut commands, window.0, current_screenshot.image.clone());
                     }
                 } else {
                     eprintln!("No screenshot data available");
@@ -351,6 +334,77 @@ fn copy_button_interaction(
     }
 }
 
+fn yes_button_interaction(
+    mut interaction_query: Query<&Interaction, (Changed<Interaction>, With<YesButton>)>,
+    current_screenshot: Option<Res<CurrentScreenshotImage>>,
+    images: Res<Assets<Image>>,
+    mut commands: Commands,
+    screenshot_window: Option<Res<ScreenshotWindowSurface>>,
+    existing_dialog: Query<Entity, With<SaveDialog>>,
+) {
+    for interaction in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                println!("Yes button was pressed - saving screenshot");
+                
+                if let Some(current_screenshot) = &current_screenshot {
+                    if let Some(image) = images.get(&current_screenshot.image) {
+                        // Remove the confirmation dialog first
+                        for entity in existing_dialog.iter() {
+                            commands.entity(entity).despawn();
+                        }
+                        
+                        // Try to save the image
+                        match super::plugin::save_image_as_png(image, &current_screenshot.output_path) {
+                            Ok(()) => {
+                                println!("Screenshot saved successfully");
+                                
+                                // Spawn save success dialog
+                                if let Some(window) = &screenshot_window {
+                                    spawn_save_success_dialog(&mut commands, window.0);
+                                }
+                            }
+                            Err(e) => {
+                                eprintln!("Failed to save screenshot: {}", e);
+                                
+                                // Spawn error dialog
+                                if let Some(window) = &screenshot_window {
+                                    spawn_error_dialog(&mut commands, window.0, &format!("Failed to save: {}", e));
+                                }
+                            }
+                        }
+                    } else {
+                        eprintln!("Screenshot image not found in assets");
+                    }
+                } else {
+                    eprintln!("No screenshot data available");
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
+fn no_button_interaction(
+    mut interaction_query: Query<&Interaction, (Changed<Interaction>, With<NoButton>)>,
+    mut commands: Commands,
+    existing_dialog: Query<Entity, With<SaveDialog>>,
+) {
+    for interaction in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                println!("No button was pressed - canceling save");
+                
+                // Remove the confirmation dialog
+                for entity in existing_dialog.iter() {
+                    commands.entity(entity).despawn();
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
 fn auto_hide_dialogs(
     mut commands: Commands,
     mut dialog_query: Query<(Entity, &mut DialogTimer), With<SaveDialog>>,
@@ -364,7 +418,140 @@ fn auto_hide_dialogs(
     }
 }
 
-fn spawn_save_dialog(commands: &mut Commands, parent: Entity) {
+fn spawn_save_confirmation_dialog(commands: &mut Commands, parent: Entity, screenshot_image: Handle<Image>) {
+    let dialog = commands.spawn((
+        Node {
+            position_type: PositionType::Absolute,
+            left: Val::Percent(50.0),
+            top: Val::Percent(50.0),
+            width: Val::Px(400.0),
+            height: Val::Px(500.0),
+            margin: UiRect {
+                left: Val::Px(-200.0), // Half of width for centering
+                top: Val::Px(-250.0),   // Half of height for centering
+                ..default()
+            },
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            flex_direction: FlexDirection::Column,
+            padding: UiRect::all(Val::Px(20.0)),
+            border: UiRect::all(Val::Px(2.0)),
+            ..default()
+        },
+        BackgroundColor(Color::srgba(0.15, 0.15, 0.15, 0.95)),
+        BorderColor(Color::srgba(0.4, 0.4, 0.4, 1.0)),
+        BorderRadius::all(Val::Px(12.0)),
+        SaveDialog,
+        DialogOverlay,
+    )).with_children(|dialog| {
+        // Screenshot preview container
+        dialog.spawn((
+            Node {
+                width: Val::Px(320.0),
+                height: Val::Px(240.0),
+                margin: UiRect::bottom(Val::Px(20.0)),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                border: UiRect::all(Val::Px(1.0)),
+                ..default()
+            },
+            BackgroundColor(Color::srgba(0.1, 0.1, 0.1, 1.0)),
+            BorderColor(Color::srgba(0.3, 0.3, 0.3, 1.0)),
+            BorderRadius::all(Val::Px(8.0)),
+        )).with_children(|preview_container| {
+            // Screenshot image
+            preview_container.spawn((
+                ImageNode::new(screenshot_image),
+                Node {
+                    width: Val::Percent(90.0),
+                    height: Val::Percent(90.0),
+                    ..default()
+                },
+            ));
+        });
+        
+        // Save confirmation message
+        dialog.spawn((
+            Text::new("Save screenshot to Screenshots folder?"),
+            TextFont {
+                font_size: 18.0,
+                ..default()
+            },
+            TextColor(Color::WHITE),
+            Node {
+                margin: UiRect::bottom(Val::Px(20.0)),
+                ..default()
+            },
+        ));
+        
+        // Button container
+        dialog.spawn((
+            Node {
+                flex_direction: FlexDirection::Row,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                column_gap: Val::Px(20.0),
+                ..default()
+            },
+        )).with_children(|buttons| {
+            // Yes button
+            buttons.spawn((
+                Button,
+                Node {
+                    width: Val::Px(80.0),
+                    height: Val::Px(40.0),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    border: UiRect::all(Val::Px(2.0)),
+                    ..default()
+                },
+                BackgroundColor(Color::srgba(0.2, 0.7, 0.2, 1.0)),
+                BorderColor(Color::srgba(0.3, 0.8, 0.3, 1.0)),
+                BorderRadius::all(Val::Px(12.0)),
+                YesButton,
+            )).with_children(|button| {
+                button.spawn((
+                    Text::new("Yes"),
+                    TextFont {
+                        font_size: 16.0,
+                        ..default()
+                    },
+                    TextColor(Color::WHITE),
+                ));
+            });
+            
+            // No button
+            buttons.spawn((
+                Button,
+                Node {
+                    width: Val::Px(80.0),
+                    height: Val::Px(40.0),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    border: UiRect::all(Val::Px(2.0)),
+                    ..default()
+                },
+                BackgroundColor(Color::srgba(0.7, 0.2, 0.2, 1.0)),
+                BorderColor(Color::srgba(0.8, 0.3, 0.3, 1.0)),
+                BorderRadius::all(Val::Px(12.0)),
+                NoButton,
+            )).with_children(|button| {
+                button.spawn((
+                    Text::new("No"),
+                    TextFont {
+                        font_size: 16.0,
+                        ..default()
+                    },
+                    TextColor(Color::WHITE),
+                ));
+            });
+        });
+    }).id();
+    
+    commands.entity(parent).add_child(dialog);
+}
+
+fn spawn_save_success_dialog(commands: &mut Commands, parent: Entity) {
     let dialog = commands.spawn((
         Node {
             position_type: PositionType::Absolute,
@@ -380,9 +567,13 @@ fn spawn_save_dialog(commands: &mut Commands, parent: Entity) {
             justify_content: JustifyContent::Center,
             align_items: AlignItems::Center,
             flex_direction: FlexDirection::Column,
+            padding: UiRect::all(Val::Px(20.0)),
+            border: UiRect::all(Val::Px(2.0)),
             ..default()
         },
-        BackgroundColor(Color::srgba(0.2, 0.2, 0.2, 0.9)),
+        BackgroundColor(Color::srgba(0.2, 0.7, 0.2, 0.9)),
+        BorderColor(Color::srgba(0.3, 0.8, 0.3, 1.0)),
+        BorderRadius::all(Val::Px(12.0)),
         SaveDialog,
         DialogOverlay,
         DialogTimer(Timer::from_seconds(3.0, TimerMode::Once)), // Auto-hide after 3 seconds
@@ -418,9 +609,12 @@ fn spawn_error_dialog(commands: &mut Commands, parent: Entity, error_message: &s
             align_items: AlignItems::Center,
             flex_direction: FlexDirection::Column,
             padding: UiRect::all(Val::Px(20.0)),
+            border: UiRect::all(Val::Px(2.0)),
             ..default()
         },
         BackgroundColor(Color::srgba(0.8, 0.2, 0.2, 0.9)),
+        BorderColor(Color::srgba(0.9, 0.3, 0.3, 1.0)),
+        BorderRadius::all(Val::Px(12.0)),
         SaveDialog,
         DialogOverlay,
         DialogTimer(Timer::from_seconds(4.0, TimerMode::Once)), // Auto-hide after 4 seconds for errors
